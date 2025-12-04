4b88d6da44d33093e562a3ab84e81c8c67207b56
Balogh Patrik <balikapatrik@gmail.com>
2025-12-04 18:25:36 +0100

    Add CreateCompetitorValidator and update Program.cs
    Introduce CreateCompetitorValidator for CompetitorRequest using
    FluentValidation, adding rules for Name, Age, and BodyWeight.
    Remove weather forecast endpoint from Program.cs. Integrate
    FluentValidation, FastEndpoints, and SwaggerGen for improved
    API handling and documentation.

---
+++ PwrApp/Endpoints/Competitor/CreateCompetitorValidator.cs
@@ -0,0 +1,20 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace PwrApp.Endpoints.Competitor;
+
+public class CreateCompetitorValidator: Validator<CompetitorRequest>
+{
+    public CreateCompetitorValidator()
+    {
+        RuleFor(x  => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .MaximumLength(100).WithMessage("Name cannot exceed 100 characters.");
+
+        RuleFor(x => x.Age)
+            .InclusiveBetween(0, 120).WithMessage("Age must be between 0 and 120.");
+
+        RuleFor(x => x.BodyWeight)
+            .InclusiveBetween(0, 200).WithMessage("Body weight must be between 0 and 200.");
+    }
+}
--- PwrApp/Program.cs
+++ PwrApp/Program.cs
@@ -1,6 +1,8 @@
 using FastEndpoints;
 using FastEndpoints.Swagger;
 using Microsoft.AspNetCore.Builder;
+using FluentValidation;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,24 +23,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 app.UseFastEndpoints().UseSwaggerGen();
 app.Run();
 
